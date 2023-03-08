ec5bac551afd5393c57c012e6027a12545597517
MikeHp2000 <121138598+MikeHp2000@users.noreply.github.com>
2023-03-08 14:23:23 -0600

    Add files via upload

---
+++ CoccaoApp Avance/ListaConImagenes/TableViewDetail.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewDetail.swift
+//  ListaConImagenes
+//
+//  Created by Miguel Angel Herrera Perez on 02/03/23.
+//
+
+import Foundation
+import UIKit
+
+class TableViewDetail:UIViewController {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var descripcion: UILabel!
+    
+    var selectedShape: Shape!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        name.text =  selectedShape.UbSalon
+        image.image = UIImage(named: selectedShape.imageUb)
+        descripcion.text = selectedShape.Salon
+    }
+}
